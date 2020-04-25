8b68c3468a2f3cc1d1d85819baa26ee927e91adb
clayton <claystamper@gmail.com>
2020-04-25 15:13:06 -0500

    getting some output for different players

--- main.cpp
+++ main.cpp
@@ -3,19 +3,49 @@
 #include "stdlib.h"
 #include "pthread.h"
 #include "semaphore.h"
+#include "string.h"
 
-static s die_1 = 0, die_2 = 0;
+
+static int die_sums[] = {0, 0, 0, 0};
 
 static pthread_mutex_t die_mutex;
 static pthread_cond_t winner_cond;
+static int seed = 0;
 
-int GetRand(int seed){
+int GetRand(){
     int max = 6;
     srand(time(NULL) * seed);
     return rand() % max;
 }
 
-void* PlayerGo(void* t_id){
+char GetName(int id){
+    switch (id) {
+        case 0:
+            return 'A';
+            break;
+        case 1:
+            return 'B';
+            break;
+        case 2:
+            return 'C';
+            break;
+        case 3:
+            return 'D';
+            break;
+    }
+}
+
+void* PlayerGo(void* id){
+
+    int myId = (long) id;
+
+
+    pthread_mutex_lock(&die_mutex);
+//    printf("Player %s had sum: %d", GetName(myId), die_sums[myId]);
+    sleep(1.0f);
+    die_sums[myId] = GetRand();
+    std::cout << "\nPlayer " << GetName(myId) << " has a last sum of " << die_sums[myId];
+    pthread_mutex_unlock(&die_mutex);
 
 }
 
@@ -27,7 +57,7 @@ int main(int argc, char* argv[])
 {
 
     if (argc != 2) {fprintf(stderr, "WARNING: %d arguments were passed! \n", argv[0]); exit(-1);}
-    const int seed = atoi(argv[1]);
+    seed = atoi(argv[1]);
 
     pthread_t threads[5];
     pthread_attr_t  attr;
@@ -35,6 +65,16 @@ int main(int argc, char* argv[])
     pthread_mutex_init(&die_mutex, 0);
     pthread_cond_init(&winner_cond, 0);
     pthread_attr_init(&attr);
+
+    for (int i = 0; i < 4; ++i) {
+        pthread_create(&threads[0], &attr, PlayerGo, (void*) i);
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        pthread_join(threads[i], 0);
+    }
+
+//    std::cout << "\nfinal id is: " << currentPlayer << "en";
 
     return 0;
 }
